0e8b09c4027575173d7eb4c177504866200faaae
lay <layroyk@163.com>
2021-10-02 20:40:00 +0800

    hadoop_ETL抽取-转换-加载

---
+++ ETL抽取-转换-加载/zz6升级版/src/com/hp/bigdata/page/ETLApp.java
@@ -0,0 +1,88 @@
+package com.hp.bigdata.page;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+import com.hp.bigdata.utils.GetPageId;
+import com.hp.bigdata.utils.LogParser;
+
+public class ETLApp {
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		Path outputPath = new Path("input/etl");
+		if (fs.exists(outputPath)) {
+				fs.delete(outputPath, true);
+		}
+		//创建job对象
+		Job job = Job.getInstance(conf);
+		//设置提交类
+		job.setJarByClass(ETLApp.class);
+		//设置map相关信息
+		job.setMapperClass(MyMapper.class);
+		job.setMapOutputKeyClass(NullWritable.class);
+		job.setMapOutputValueClass(Text.class);
+		
+		//设置输入输出路径
+		FileInputFormat.setInputPaths(job, new Path("input/trackinfo_20130721.txt"));
+		FileOutputFormat.setOutputPath(job, new Path("input/etl"));
+		//提交
+		job.waitForCompletion(true);
+	}
+	static class MyMapper extends Mapper<LongWritable, Text, NullWritable, Text>{
+		//初始化
+		private LogParser parser;
+		//重写setup方法
+		@Override
+		protected void setup(Mapper<LongWritable, Text, NullWritable, Text>.Context context)
+				throws IOException, InterruptedException {
+			// TODO Auto-generated method stub
+			parser=new LogParser();
+		}
+		//重写map方法
+		@Override
+		protected void map(LongWritable key, Text value,
+				org.apache.hadoop.mapreduce.Mapper.Context context)
+				throws IOException, InterruptedException {
+			// TODO Auto-generated method stub
+			String line = value.toString();
+			Map<String,String> longInfo = parser.parse(line);
+			//获取一行有效字段
+			String ip = longInfo.get("ip");
+			String url = longInfo.get("url");
+			String sessionId = longInfo.get("sessionId");
+			String time = longInfo.get("time");
+			String country = longInfo.get("country") == null ? "-" : longInfo.get("country");
+			String province = longInfo.get("province") == null ? "-" : longInfo.get("province");
+			String city = longInfo.get("city") == null ? "-" : longInfo.get("city");
+			String pageId = GetPageId.getPageId(url) == null ? "-" : GetPageId.getPageId(url);
+			
+			//创建缓冲区
+			StringBuilder builder = new StringBuilder();
+			builder.append(ip+"\t");
+			builder.append(url+"\t");
+			builder.append(sessionId+"\t");
+			builder.append(time+"\t");
+			builder.append(country+"\t");
+			builder.append(province+"\t");
+			builder.append(city+"\t");
+			builder.append(pageId+"\t");
+			
+			//写入上写文
+			context.write(NullWritable.get(), new Text(builder.toString()));
+			
+			}
+	}
+}
---
+++ ETL抽取-转换-加载/zz6升级版/src/com/hp/bigdata/page/PageStatApp.java
@@ -0,0 +1,95 @@
+package com.hp.bigdata.page;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import com.hp.bigdata.page.PVStatApp.MyMapper;
+import com.hp.bigdata.page.PVStatApp.MyReducer;
+import com.hp.bigdata.utils.GetPageId;
+import com.hp.bigdata.utils.LogParser;
+
+public class PageStatApp {
+	public static void main(String[] args) throws Exception {
+		//创建Configuration对象
+				Configuration conf = new Configuration();
+				//判断输出路径是否存在，存在删除
+				FileSystem fs = FileSystem.get(conf);
+				Path outputPath = new Path("output");
+				if (fs.exists(outputPath)) {
+						fs.delete(outputPath, true);
+				}
+				//创建job对象
+				Job job = Job.getInstance(conf);
+				//设置提交类
+				job.setJarByClass(PageStatApp.class);
+				//设置map相关信息
+				job.setMapperClass(MyMapper.class);
+				job.setMapOutputKeyClass(Text.class);
+				job.setMapOutputValueClass(LongWritable.class);
+				//设置reduce相关信息
+				job.setReducerClass(MyReducer.class);
+				job.setOutputKeyClass(Text.class);
+				job.setOutputValueClass(LongWritable.class);
+				//设置输入输出路径
+				FileInputFormat.setInputPaths(job, new Path("input/etl"));
+				FileOutputFormat.setOutputPath(job, new Path("output"));
+				//提交
+				job.waitForCompletion(true);
+	}
+	static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+		private LogParser parser;
+		private LongWritable ONE = new LongWritable(1);
+		private Text KEY =new Text("KEY");
+		@Override
+		protected void setup(Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			// TODO Auto-generated method stub
+			//实例化
+			parser=new LogParser();
+		}
+		@Override
+		protected void map(LongWritable key, Text value,
+				Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			// TODO Auto-generated method stub
+			//读取一行数据
+			String line = value.toString();
+			//解析url
+			Map<String,String> logInfo = parser.parse2(line);
+			
+			//获取url
+			String url = logInfo.get("url");
+			//获取pageID
+			String pageId = GetPageId.getPageId(url);
+			//写入上下文
+			context.write(new Text(pageId), ONE);
+			
+		}
+	}
+	static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+		@Override
+		protected void reduce(Text key, Iterable<LongWritable> values,
+				Reducer<Text, LongWritable, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			// TODO Auto-generated method stub
+			long sum=0;
+			for (LongWritable value : values) {
+				sum++;
+			}
+			context.write(key, new LongWritable(sum));
+		}
+	}
+}
